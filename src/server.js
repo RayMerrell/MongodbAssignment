60a7bd60ca866f97cb2846d15f531b1cc5211507
Ray Merrell <RayMerrell@outlook.com>
2023-04-12 14:33:13 +0100

    Connection

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
+require("./db/connection");
+
 //set up basic server
 const express = require ("express");
-
 const app = express();
-
 app.use(express.json());
+
 
 app.listen(5001, ()=>console.log("Server is listening"));
